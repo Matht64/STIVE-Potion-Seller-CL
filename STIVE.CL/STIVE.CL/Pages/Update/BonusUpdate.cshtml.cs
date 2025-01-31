7745f8a90d7707a44c5d9d635d825ab7dd146319
TerryGyselings <terry.gyselings@viacesi.fr>
2025-01-31 16:21:51 +0100

    add bonusupdate

---
+++ STIVE.CL/STIVE.CL/Pages/Update/BonusUpdate.cshtml.cs
@@ -0,0 +1,68 @@
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace STIVE.CL.Pages;
+
+public class BonusUpdateModel : PageModel
+{
+    private readonly HttpClient _httpClient;
+
+    public BonusUpdateModel(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    [BindProperty(SupportsGet = true)]
+    public int Id { get; set; }
+    [BindProperty]
+    public BonusDto Bonus { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        if (Id <= 0)
+            return NotFound();
+        
+        var response = await _httpClient.GetAsync($"http://localhost:5294/api/Bonus/getById/{Id}");
+        
+        if (!response.IsSuccessStatusCode)
+            return NotFound();
+        
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+        Bonus = JsonSerializer.Deserialize<BonusDto>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+            return Page();
+        
+        var jsonData = JsonSerializer.Serialize(Bonus);
+        var content  = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var response = await _httpClient.PutAsync($"http://localhost:5294/api/Bonus/update/{Bonus.BonusId}", content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            ModelState.AddModelError(string.Empty, "Potion update failed");
+            return Page();
+        }
+        
+        return RedirectToPage("/Admin");
+    }
+}
+
+public class BonusDto
+{
+    [JsonPropertyName("id")]
+    public int BonusId { get; set; }
+    [JsonPropertyName("name")]
+    public string BonusName { get; set; }
+    [JsonPropertyName("duration")]
+    public int BonusDuration { get; set; }
+    [JsonPropertyName("price")]
+    public float BonusPrice { get; set; }
+}
--- STIVE.CL/STIVE.CL/obj/Debug/net8.0/STIVE.CL.AssemblyInfo.cs
+++ STIVE.CL/STIVE.CL/obj/Debug/net8.0/STIVE.CL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STIVE.CL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c84a73f09222ee8125f3ebcc50a64368717d3bec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+751f245545cf3e9ee4efde0fbf22c16a0c15c99d")]
 [assembly: System.Reflection.AssemblyProductAttribute("STIVE.CL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STIVE.CL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
