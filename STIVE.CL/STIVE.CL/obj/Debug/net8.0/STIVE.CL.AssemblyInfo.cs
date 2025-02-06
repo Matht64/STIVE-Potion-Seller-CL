f90efe980034e81e6198a5e579e942bf0130692c
TerryGyselings <terry.gyselings@viacesi.fr>
2025-02-06 19:20:37 +0100

    add filter

--- STIVE.CL/STIVE.CL/obj/Debug/net8.0/STIVE.CL.AssemblyInfo.cs
+++ STIVE.CL/STIVE.CL/obj/Debug/net8.0/STIVE.CL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STIVE.CL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+612a88d772dd24b8c739042a306136cbe3dc6148")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063841a8c3b5e523c8ac983ccc4ac5d6419bf823")]
 [assembly: System.Reflection.AssemblyProductAttribute("STIVE.CL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STIVE.CL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
